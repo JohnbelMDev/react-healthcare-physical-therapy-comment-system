70d0b381c8be49431e1f94e8ee06cad4f4023f07
JohnbelMDev <johnbel458@gmail.com>
2025-10-10 13:38:31 -0400

    added comment for description

--- react-physical-therapy-comment-2025/src/App.tsx
+++ react-physical-therapy-comment-2025/src/App.tsx
@@ -4,31 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div >
+      <h1>React Physical Therapy Comment 2025</h1>
+      <h2>Welcome to React Physical Therapy Comment 2025</h2>
+      <p>This is a sample application to demonstrate a React setup.</p>
+      <form action=""><input type="text" /></form>
+    </div>
   )
 }
 
